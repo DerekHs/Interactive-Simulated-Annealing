84b77cf4c403b40054ca9d5678735b454eb297f3
Derek Hsiao <hsiaoderek@berkeley.edu>
2017-11-19 16:01:07 -0800

    can now read inputs

--- src/Solver.java
+++ src/Solver.java
@@ -1,14 +1,37 @@
 /**
  * Created by derek on 11/19/17.
  */
+import java.io.*;
+
 public class Solver {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Assignment ass = new Assignment("phase2_inputs/inputs20/input20_0.in");
     }
 }
 
 class Assignment {
-    Assignment(int num_wizards, file) {
-
+    /* reads in a file, and creates a system to store
+    a mapping from wizard to age rank
+    */
+    Assignment(String fileName) {
+        String line = null;
+        try {
+            FileReader reader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("unable to read file " + fileName);
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
     }
 }
